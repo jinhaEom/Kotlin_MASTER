f91b1ec6a00c0abc27cce530ced9b2e2d11294fd
jinhaEom <umjin98@naver.com>
2022-02-09 15:49:53 +0900

    commit

--- UsedGoods_Trade/app/src/main/java/bu/ac/kr/usedgoods_trade/chatlist/ChatListFragment.kt
+++ UsedGoods_Trade/app/src/main/java/bu/ac/kr/usedgoods_trade/chatlist/ChatListFragment.kt
@@ -1,7 +1,72 @@
 package bu.ac.kr.usedgoods_trade.chatlist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import bu.ac.kr.usedgoods_trade.R
+import bu.ac.kr.usedgoods_trade.databinding.FragmentChatlistBinding
+import bu.ac.kr.usedgoods_trade.mypage.DBKey.Companion.CHILD_CHAT
+import bu.ac.kr.usedgoods_trade.mypage.DBKey.Companion.DB_USERS
+import com.bumptech.glide.disklrucache.DiskLruCache
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ChatListFragment: Fragment(R.layout.fragment_chatlist) {
+
+    private var binding : FragmentChatlistBinding? = null
+    private lateinit var chatListAdapter : ChatListAdapter
+    private val chatRoomList = mutableListOf<ChatListItem>()
+
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentChatListBinding = FragmentChatlistBinding.bind(view)
+        binding = fragmentChatListBinding
+
+        chatListAdapter = ChatListAdapter(onItemClicked = {
+            //채팅방으로 이동하는 코드
+        })
+
+        chatRoomList.clear()
+
+        fragmentChatListBinding.chatListRecyclerView.adapter = chatListAdapter
+        fragmentChatListBinding.chatListRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        if(auth.currentUser == null){
+            return
+        }
+        val chatDB = Firebase.database.reference.child(DB_USERS).child(auth.currentUser!!.uid).child(CHILD_CHAT)
+        chatDB.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach{
+                    val model = it.getValue(ChatListItem::class.java)
+                    model ?: return
+
+                    chatRoomList.add(model)
+                }
+                chatListAdapter.submitList(chatRoomList)
+                chatListAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        chatListAdapter.notifyDataSetChanged()
+    }
 }
