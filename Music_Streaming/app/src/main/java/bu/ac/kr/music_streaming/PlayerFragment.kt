aed4271f6ed9d13dd38495b4ec7ab2f52a3296ce
jinhaEom <umjin98@naver.com>
2022-03-09 13:59:37 +0900

    playList클릭시 화면전환 구현

--- Music_Streaming/app/src/main/java/bu/ac/kr/music_streaming/PlayerFragment.kt
+++ Music_Streaming/app/src/main/java/bu/ac/kr/music_streaming/PlayerFragment.kt
@@ -2,7 +2,9 @@ package bu.ac.kr.music_streaming
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import bu.ac.kr.music_streaming.databinding.FragmentPlayerBinding
 import bu.ac.kr.music_streaming.service.MusicDto
 import bu.ac.kr.music_streaming.service.MusicService
 import com.google.android.exoplayer2.util.Log
@@ -13,12 +15,30 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
+    private var binding : FragmentPlayerBinding? = null
+    private var isWatchingPlayListView = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
+        binding = fragmentPlayerBinding
+        initPlayListButton(fragmentPlayerBinding)
+
         getVideoListFromServer()
     }
+
+    private fun initPlayListButton(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playlistImageView.setOnClickListener {
+            //TODO 만약에 서버에서 데이터가 다 불러오지 않은 형태일때 예외처리 
+            fragmentPlayerBinding.playerViewGroup.isVisible = isWatchingPlayListView
+
+            fragmentPlayerBinding.playListViewGroup.isVisible = isWatchingPlayListView.not()
+
+            isWatchingPlayListView = !isWatchingPlayListView
+        }
+    }
+
     private fun getVideoListFromServer(){
         val retrofit = Retrofit.Builder()
             .baseUrl("https://run.mocky.io")
