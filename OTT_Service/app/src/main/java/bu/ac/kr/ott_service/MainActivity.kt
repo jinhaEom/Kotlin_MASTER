d94a7173b3384ffc20a681c4e92fffb33272894b
jinhaEom <umjin98@naver.com>
2022-03-25 15:30:04 +0900

    motionLayout 생성

--- OTT_Service/app/src/main/java/bu/ac/kr/ott_service/MainActivity.kt
+++ OTT_Service/app/src/main/java/bu/ac/kr/ott_service/MainActivity.kt
@@ -1,11 +1,50 @@
 package bu.ac.kr.ott_service
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import androidx.constraintlayout.motion.widget.MotionLayout
+import bu.ac.kr.ott_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
+
+    private var isGatheringMotionAnimating: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if(binding.scrollView.scrollY > 150f.dpToPx(this).toInt()){
+                if(isGatheringMotionAnimating.not()){
+                    binding.gatheringDigitalThingsLayout.transitionToEnd()
+                }
+
+            }else{
+                if(!isGatheringMotionAnimating){
+                    binding.gatheringDigitalThingsLayout.transitionToStart()
+                }
+            }
+        }
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object : MotionLayout.TransitionListener{
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                isGatheringMotionAnimating = true
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                isGatheringMotionAnimating = false
+            }
+
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) =Unit
+
+        })
+
     }
+    fun Float.dpToPx(context: Context): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this,context.resources.displayMetrics)
 }
