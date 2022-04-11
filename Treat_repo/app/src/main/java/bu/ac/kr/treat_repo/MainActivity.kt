454b23feb0e3da46f5a5eff9e0ada195815f1491
jinhaEom <umjin98@naver.com>
2022-04-11 15:49:17 +0900

    github 페이지 연결

--- Treat_repo/app/src/main/java/bu/ac/kr/treat_repo/MainActivity.kt
+++ Treat_repo/app/src/main/java/bu/ac/kr/treat_repo/MainActivity.kt
@@ -1,7 +1,11 @@
 package bu.ac.kr.treat_repo
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.viewbinding.BuildConfig
 import bu.ac.kr.treat_repo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +18,32 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+    private fun initViews() = with(binding){
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+    private fun loginGithub(){
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", bu.ac.kr.treat_repo.BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(this,loginUri)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let{
+
+        }
     }
 }
