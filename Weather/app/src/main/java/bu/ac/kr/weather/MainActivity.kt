37a255517beafa6a998085971c59d7bb3d34bbc2
jinhaEom <umjin98@naver.com>
2022-04-15 16:28:09 +0900

    permission 권한,내위치 정보 가져오기

--- Weather/app/src/main/java/bu/ac/kr/weather/MainActivity.kt
+++ Weather/app/src/main/java/bu/ac/kr/weather/MainActivity.kt
@@ -1,11 +1,86 @@
 package bu.ac.kr.weather
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import bu.ac.kr.weather.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var cancellationTokenSource: CancellationTokenSource? = null
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initVariables()
+        requestLocationPermissions()
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (!locationPermissionGranted) {
+            finish()
+        } else {
+            //fetchData
+            cancellationTokenSource = CancellationTokenSource()
+
+            fusedLocationProviderClient.getCurrentLocation(
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+            ).addOnSuccessListener { location ->
+                binding.textView.text = "${location.latitude}, ${location.longitude}"
+
+
+            }
+        }
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+    }
+
+    private fun requestLocationPermissions() {
+        //권한요청
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
