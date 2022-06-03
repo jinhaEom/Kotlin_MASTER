2d1624e959169b76bb20ffb4648acbbd7595348c
jinhaEom <umjin98@naver.com>
2022-06-03 15:29:01 +0900

    앱 구현 pause

---
+++ Treat_repo/app/src/main/java/bu/ac/kr/treat_repo/data/response/database/SimpleGithubDatabase.kt
@@ -0,0 +1,14 @@
+package bu.ac.kr.treat_repo.data.response.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import bu.ac.kr.treat_repo.data.response.dao.SearchHistoryDao
+
+import bu.ac.kr.treat_repo.data.response.entity.GithubRepoEntity
+
+@Database(entities = [GithubRepoEntity::class], version = 1)
+abstract class SimpleGithubDatabase : RoomDatabase() {
+
+    abstract fun searchHistoryDao(): SearchHistoryDao
+
+}
--- Treat_repo/app/src/main/java/bu/ac/kr/treat_repo/data/response/entity/GithubRepoEntity.kt
+++ Treat_repo/app/src/main/java/bu/ac/kr/treat_repo/data/response/entity/GithubRepoEntity.kt
@@ -1,14 +1,16 @@
 package bu.ac.kr.treat_repo.data.response.entity
 
 import androidx.room.Embedded
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-data class GithubRepoEntity (
+@Entity(tableName = "GithubRepository")
+data class GithubRepoEntity(
     val name: String,
     @PrimaryKey val fullName: String,
     @Embedded val owner: GithubOwner,
-    val description : String?,
-    val language : String,
-    val updatedAt : String,
+    val description: String?,
+    val language: String?,
+    val updatedAt: String,
     val stargazersCount: Int
-        )
+)
