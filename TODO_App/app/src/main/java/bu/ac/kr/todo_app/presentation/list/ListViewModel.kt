56612fd17c9e887e835b96769b2302259147ca93
jinhaEom <umjin98@naver.com>
2022-06-13 16:51:55 +0900

    todo 리스트

---
+++ TODO_App/app/src/main/java/bu/ac/kr/todo_app/presentation/list/ListViewModel.kt
@@ -0,0 +1,13 @@
+package bu.ac.kr.todo_app.presentation.list
+
+/**
+ *필요한 UseCase
+ * 1. GetToDoListUseCase
+ * 2. UpdateToDoUseCase
+ * 3. DeleteAllToDoItemUsecase
+ */
+internal class ListViewModel (
+
+        ){
+
+}
---
+++ TODO_App/app/src/test/java/bu/ac/kr/todo_app/viewmodel/todo/ListViewModelTest.kt
@@ -0,0 +1,27 @@
+package bu.ac.kr.todo_app.viewmodel.todo
+
+import org.junit.Before
+
+/**
+ * [ListViewModel]을 테스트하기 위한 Unit Test Class
+ *
+ * 1. initData()
+ * 2. test viewMdel fetch
+ * 3. test Item Update
+ * 4. test Item Delete All
+ */
+
+
+class ListViewModelTest{
+
+    /**
+     * 필요한 UseCase들
+     * 1. InsertToDoListUseCase
+     * 2. GetToDoItemUseCase
+     */
+    @Before
+    fun init(){
+        initData()
+    }
+    private fun initData() =
+}
