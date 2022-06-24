d06c87a435dd534c122017160f2b7a9ec13e5a48
jinhaEom <umjin98@naver.com>
2022-06-24 17:17:23 +0900

    조회 메소드

--- SQLite_Test/app/src/main/java/bu/ac/kr/sqlite_test/SqliteHelper.kt
+++ SQLite_Test/app/src/main/java/bu/ac/kr/sqlite_test/SqliteHelper.kt
@@ -29,5 +29,29 @@ class SqliteHelper(context: Context, name: String, version: Int) :
         wd.insert("memo",null,values)
         wd.close()
     }
+    fun selectMemo():MutableList<Memo>{ //조회 메소드
+        val list= mutableListOf<Memo>() // 메소드의 가장 윗줄에서 반환할 값을 선언 / 가장 아랫줄에서 반환
+
+        val select = "select * from memo"  // 메모 전체 데이터를 조회하는 쿼리
+        val rd = readableDatabase// 읽기전용 db를 변수에
+
+        val cursor = rd.rawQuery(select, null)  //커서 : 데이터 셋을 처리할 때 현재 위치를 포함하는 데이터 요소
+
+        while(cursor.moveToNext()){
+            val noIdx = cursor.getColumnIndex("no") // 1. 데이블에서 no 컬럼의 순서
+            val contentIdx = cursor.getColumnIndex("content") // 2
+            val dateIdx = cursor.getColumnIndex("datetime") //3
+
+            val no = cursor.getLong(noIdx) // 값은 위에서 저장해 둔 컬럼의 위치로 가져옴.
+            val content = cursor.getString(contentIdx)
+            val datetime = cursor.getLong(dateIdx)
+            list.add(Memo(no, content,datetime))
+
+        }
+        cursor.close()
+        rd.close()  // close() 미호출 시 반환되지 않아 메모리 자원 낭비
+
+        return list
+    }
 }
 data class Memo(var no: Long?,var content:String, var datetime: Long)
