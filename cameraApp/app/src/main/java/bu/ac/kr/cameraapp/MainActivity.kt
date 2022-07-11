897eab3ee001ff53276f6b281d28296393b5e1fa
jinahaEom <umjin98@naver.com>
2022-07-11 21:02:44 +0900

    새로운파일 자동생성후 이미지저장

--- cameraApp/app/src/main/java/bu/ac/kr/cameraapp/MainActivity.kt
+++ cameraApp/app/src/main/java/bu/ac/kr/cameraapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.display.DisplayManager
+import android.media.MediaScannerConnection
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -14,6 +16,11 @@ import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import bu.ac.kr.cameraapp.databinding.ActivityMainBinding
+import bu.ac.kr.cameraapp.util.PathUtil
+import java.io.File
+import java.io.FileNotFoundException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -21,63 +28,67 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var cameraExecutor : ExecutorService
-    private val cameraMainExecutor by lazy { ContextCompat.getMainExecutor(this)}
+    private lateinit var cameraExecutor: ExecutorService
+    private val cameraMainExecutor by lazy { ContextCompat.getMainExecutor(this) }
 
-    private val cameraProviderFuture by lazy { ProcessCameraProvider.getInstance(this)} // 카메라 얻어오면 이후 실행 리스너 등록
+    private val cameraProviderFuture by lazy { ProcessCameraProvider.getInstance(this) } // 카메라 얻어오면 이후 실행 리스너 등록
 
-    private lateinit var imageCapture : ImageCapture
-    private val displayManager by lazy{
+    private lateinit var imageCapture: ImageCapture
+    private val displayManager by lazy {
         getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
     }
     private var displayId: Int = -1
 
     private var camera: Camera? = null
-    private val displayListener = object : DisplayManager.DisplayListener{
+    private var isCapturing: Boolean = false
+    private val displayListener = object : DisplayManager.DisplayListener {
         override fun onDisplayAdded(p0: Int) = Unit
 
         override fun onDisplayRemoved(p0: Int) = Unit
 
         override fun onDisplayChanged(p0: Int) {
-            if(this@MainActivity.displayId == displayId){
+            if (this@MainActivity.displayId == displayId) {
 
             }
         }
 
     }
 
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       binding = ActivityMainBinding.inflate(layoutInflater)
-       setContentView(binding.root)
-       if (allPermissionsGranted()) {
-           startCamera(binding.viewFinder)
-       } else {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (allPermissionsGranted()) {
+            startCamera(binding.viewFinder)
+        } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
             )
-       }
-   }
+        }
+    }
+
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
-            baseContext,it
+            baseContext, it
         ) == PackageManager.PERMISSION_GRANTED
     }
-    private fun startCamera(viewFinder : PreviewView){
-        displayManager.registerDisplayListener(displayListener , null)
+
+    private fun startCamera(viewFinder: PreviewView) {
+        displayManager.registerDisplayListener(displayListener, null)
         cameraExecutor = Executors.newSingleThreadExecutor()
         viewFinder.postDelayed({
             displayId = viewFinder.display.displayId
             bindCameraUseCase()
-        },10)
+        }, 10)
     }
-    private fun bindCameraUseCase() = with(binding){
+
+    private fun bindCameraUseCase() = with(binding) {
         val rotation = viewFinder.display.rotation
-        val cameraSelector= CameraSelector.Builder().requireLensFacing(LENS_FACING).build()
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(LENS_FACING).build()
 
         cameraProviderFuture.addListener({
-            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder().apply{
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().apply {
                 setTargetAspectRatio(AspectRatio.RATIO_4_3)
                 setTargetRotation(rotation)
 
@@ -89,34 +100,88 @@ class MainActivity : AppCompatActivity() {
                 .setFlashMode(ImageCapture.FLASH_MODE_AUTO)
 
             imageCapture = imageCaptureBuilder.build()
-            try{
+            try {
                 cameraProvider.unbindAll()  //  기존에 바인딩 되어있는 카메라는 해제
                 camera = cameraProvider.bindToLifecycle(
-                    this@MainActivity, cameraSelector,preview, imageCapture
+                    this@MainActivity, cameraSelector, preview, imageCapture
                 )
                 preview.setSurfaceProvider(viewFinder.surfaceProvider)
-            }catch(e: Exception){
+                bindCaptureListener()
+            } catch (e: Exception) {
                 e.printStackTrace()
             }
-        }, cameraExecutor)
+        }, cameraMainExecutor)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CODE_PERMISSIONS){
-            if(allPermissionsGranted()){
-                startCamera(binding.viewFinder)
-            }else{
-                Toast.makeText(this,"카메라 권한이 없습니다.",Toast.LENGTH_SHORT).show()
+    private fun bindCaptureListener() = with(binding) {
+        captureButton.setOnClickListener {
+            if (isCapturing.not()) {
+                isCapturing = true
+                captureCamera()
             }
         }
     }
 
-    companion object{
+    private fun updateSavedImageContent() {
+        contentUri?.let {
+            isCapturing = try {
+                val file = File(PathUtil.getPath(this, it) ?: throw FileNotFoundException())
+                MediaScannerConnection.scanFile(this,
+                    arrayOf(file.path),
+                    arrayOf("image/jpeg"),
+                    null) //외부에서도 파일 읽히게 하기
+                false
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
+    }
 
+    private var contentUri: Uri? = null
+    private fun captureCamera() {
+        if (::imageCapture.isInitialized.not()) return
+        val photoFile = File(
+            PathUtil.getOutputDirectory(this),
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.KOREA
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(outputOptions,
+            cameraExecutor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
+                    contentUri = savedUri
+                    updateSavedImageContent()
+                }
 
+                override fun onError(e: ImageCaptureException) {
+                    e.printStackTrace()
+                    isCapturing = false
+                }
+
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera(binding.viewFinder)
+            } else {
+                Toast.makeText(this, "카메라 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private val LENS_FACING : Int = CameraSelector.LENS_FACING_BACK  //후면카메라
+        private val LENS_FACING: Int = CameraSelector.LENS_FACING_BACK  //후면카메라
     }
 }
