61b143df4b44b611b8b720131473f1b1843acd3d
jinahaEom <umjin98@naver.com>
2022-07-13 16:06:22 +0900

    오류발생으로 앱 재생성

---
+++ cameraApp/app/src/main/java/bu/ac/kr/cameraapp/ImageListActivity.kt
@@ -0,0 +1,11 @@
+package bu.ac.kr.cameraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_list)
+    }
+}
