c6f0663d8fe6deeaba9f79af5d5e6834af07bc57
jinahaEom <umjin98@naver.com>
2022-07-16 10:18:28 +0900

    이미지 삭제 기능 구현

--- cameraApp/app/src/main/java/bu/ac/kr/cameraapp/ImageListActivity.kt
+++ cameraApp/app/src/main/java/bu/ac/kr/cameraapp/ImageListActivity.kt
@@ -2,12 +2,17 @@ package bu.ac.kr.cameraapp
 
 import android.app.Activity
 import android.content.Intent
+import android.media.MediaScannerConnection
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import bu.ac.kr.cameraapp.adapter.ImageViewPagerAdapter
 import bu.ac.kr.cameraapp.databinding.ActivityImageListBinding
+import bu.ac.kr.cameraapp.util.PathUtil
+import java.io.File
+import java.io.FileNotFoundException
 
 class ImageListActivity : AppCompatActivity() {
     companion object{
@@ -46,5 +51,30 @@ class ImageListActivity : AppCompatActivity() {
                 toolbar.title = getString(R.string.images_page, position+1 ,imageViewPagerAdapter.uriList.size)
             }
         })
+        binding.deleteButton.setOnClickListener {
+            removeImage(uriList[imageViewPager.currentItem])
+        }
+    }
+    private fun removeImage(uri:Uri){
+
+        try{
+            val file= File(PathUtil.getPath(this,uri)?: throw FileNotFoundException())
+            file.delete()
+            imageViewPagerAdapter.uriList.let{
+                val imageList = it.toMutableList()
+                imageList.remove(uri)
+                imageViewPagerAdapter.uriList = imageList
+                imageViewPagerAdapter.notifyDataSetChanged()
+            }
+            MediaScannerConnection.scanFile(this, arrayOf(file.path), arrayOf("image/jpeg"), null)
+            binding.indicator.setViewPager(binding.imageViewPager)
+            if(imageViewPagerAdapter.uriList.isEmpty()){
+                Toast.makeText(this,"삭제할 수 있는 이미지가 없습니다.",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }catch (e: FileNotFoundException){
+            e.printStackTrace()
+            Toast.makeText(this,"이미지가 존재하지 않습니다.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
