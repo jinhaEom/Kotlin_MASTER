096df45cce4edb6563d6e20489dfec14e2b76a12
jinahaEom <umjin98@naver.com>
2022-08-07 15:42:59 +0900

    로그인 버튼 클릭시 깃헙 화면이동 구현

--- Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/MainActivity.kt
+++ Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/MainActivity.kt
@@ -1,7 +1,10 @@
 package bu.ac.kr.github_repository_app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
 import bu.ac.kr.github_repository_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initViews()
+
+    }
+    private fun initViews() = with(binding){
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+    //todo https:///github.com/login/oiauth/authorize?cliend_id=asdfasdf
+    private fun loginGithub(){
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id",BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+
+        CustomTabsIntent.Builder().build().also{
+            it.launchUrl(this,loginUri)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        intent?.data?.getQueryParameter("code")?.let{
+            //todo getAccessToken
+        }
     }
 }
