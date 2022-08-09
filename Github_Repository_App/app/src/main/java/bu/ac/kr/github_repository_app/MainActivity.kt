62cd488a7b027422a9f728120cfc6bb16cb986ae
jinahaEom <umjin98@naver.com>
2022-08-09 11:44:11 +0900

    mocking 데이터 생성

--- Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/MainActivity.kt
+++ Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/MainActivity.kt
@@ -1,30 +1,21 @@
 package bu.ac.kr.github_repository_app
 
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.core.view.isGone
+import android.os.Bundle
+import bu.ac.kr.github_repository_app.data.entity.GithubOwner
+import bu.ac.kr.github_repository_app.data.entity.GithubRepoEntity
 import bu.ac.kr.github_repository_app.databinding.ActivityMainBinding
-import bu.ac.kr.github_repository_app.utility.AuthTokenProvider
-import bu.ac.kr.github_repository_app.utility.RetrofitUtil
 import kotlinx.coroutines.*
+import java.util.*
 import kotlin.coroutines.CoroutineContext
-
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
 
-    private lateinit var binding: ActivityMainBinding
+    val job = Job()
+    private lateinit var binding : ActivityMainBinding
 
-    private val authTokenProvider by lazy{ AuthTokenProvider(this)}
-
-    val job : Job = Job()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,70 +23,28 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initViews()
+        launch{
+            val dataJob = addMockData()
+        }
 
     }
 
-    private fun initViews() = with(binding) {
-        loginButton.setOnClickListener {
-            loginGithub()
+    private suspend fun addMockData() = withContext(Dispatchers.IO) {
+        val mockData = (0 until 9).map {
+            GithubRepoEntity(
+                name = "repo $it",
+                fullName = "name $it",
+                owner = GithubOwner(
+                    "login",
+                    "avatarUrl"
+                ),
+                description = null,
+                language = null,
+                updatedAt = Date().toString(),
+                stargazersCount = it
+            )
         }
     }
 
-    //todo https:///github.com/login/oiauth/authorize?cliend_id=asdfasdf
-    private fun loginGithub() {
-        val loginUri = Uri.Builder().scheme("https").authority("github.com")
-            .appendPath("login")
-            .appendPath("oauth")
-            .appendPath("authorize")
-            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
-            .build()
 
-        CustomTabsIntent.Builder().build().also {
-            it.launchUrl(this, loginUri)
-        }
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-
-        intent?.data?.getQueryParameter("code")?.let {
-            //todo getAccessToken
-            launch(coroutineContext){
-                showProgress()
-                val getAccessTokenJob = getAccessToken(it)
-                dismissProgress()
-            }
-        }
-    }
-    private suspend fun showProgress() = withContext(coroutineContext){
-        with(binding){
-            loginButton.isGone = true
-            progressBar.isGone = false
-            progressTextView.isGone = false
-        }
-    }
-    private suspend fun dismissProgress() = withContext(coroutineContext){
-        with(binding){
-            loginButton.isGone = false
-            progressBar.isGone = true
-            progressTextView.isGone = true
-        }
-    }
-    private suspend fun getAccessToken(code : String) = withContext(Dispatchers.IO) {
-        val response = RetrofitUtil.authApiService.getAccessToken(
-            clientId = BuildConfig.GITHUB_CLIENT_ID,
-            clientSecret = BuildConfig.GITHUB_CLIENT_SECRET,
-            code = code
-        )
-        if (response.isSuccessful) {
-            val accessToken = response.body()?.accessToken ?: ""
-            Log.e("accessToken", accessToken)
-            if(accessToken.isNotEmpty()){
-                authTokenProvider.updateToken(accessToken)
-            }else{
-                Toast.makeText(this@MainActivity,"access 토큰이 존재하지 않습니다", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
 }
---
+++ Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/data/entity/GithubOwner.kt
@@ -0,0 +1,6 @@
+package bu.ac.kr.github_repository_app.data.entity
+
+data class GithubOwner (
+    val login : String,
+    val avatarUrl : String
+)
