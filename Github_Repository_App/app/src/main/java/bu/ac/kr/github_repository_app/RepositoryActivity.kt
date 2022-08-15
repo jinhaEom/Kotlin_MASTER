e368b8fbbadd273e526fb258c2d5d3e5fcc6ffaa
jinahaEom <umjin98@naver.com>
2022-08-15 19:04:23 +0900

    data set 및 progressbar 적용

--- Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/RepositoryActivity.kt
+++ Github_Repository_App/app/src/main/java/bu/ac/kr/github_repository_app/RepositoryActivity.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.view.isGone
 import bu.ac.kr.github_repository_app.data.entity.GithubRepoEntity
 import bu.ac.kr.github_repository_app.databinding.ActivityRepositoryBinding
+import bu.ac.kr.github_repository_app.extensions.loadCenterInside
 import bu.ac.kr.github_repository_app.utility.RetrofitUtil
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
@@ -45,9 +47,11 @@ class RepositoryActivity : AppCompatActivity(), CoroutineScope {
             loadRepository(repositoryOwner, repositoryName)?.let{
                 setData(it)
             }?: run{
-
+                toast("Repository 정보가 없습니다.")
+                finish()
             }
         }
+        showLoading(true)
     }
     private suspend fun loadRepository(repositoryOwner : String, repositoryName : String) : GithubRepoEntity? =
         withContext(coroutineContext){
@@ -69,8 +73,24 @@ class RepositoryActivity : AppCompatActivity(), CoroutineScope {
             repositoryEntity
         }
 
-    private fun setData(githubREpoEntity : GithubRepoEntity) = with(binding){
+    private fun setData(githubRepoEntity : GithubRepoEntity) = with(binding){
+        showLoading(false)
+        ownerProfileImageView.loadCenterInside(githubRepoEntity.owner.avatarUrl,42f)
+        ownerNameAndRepoNameTextView.text = "${githubRepoEntity.owner.login}/ ${githubRepoEntity.name}"
+        stargazersCountText.text= githubRepoEntity.stargazersCount.toString()
+        githubRepoEntity.language?.let{ language ->
+            languageText.isGone = false
+            languageText.text = language
+        } ?: kotlin.run {
+            languageText.isGone = true
+            languageText.text = ""
+        }
+        descriptionTextView.text = githubRepoEntity.description
+        updateTimeTextView.text = githubRepoEntity.updatedAt
 
+    }
+    private fun showLoading(isShown : Boolean) = with(binding){
+        progressBar.isGone = isShown.not()
     }
 
         private fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
