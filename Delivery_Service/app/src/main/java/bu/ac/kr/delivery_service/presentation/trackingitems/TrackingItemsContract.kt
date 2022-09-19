9f14161165472888ef9d2c3b73978af0174016c3
jinahaEom <umjin98@naver.com>
2022-09-19 15:08:43 +0900

    아이템 layout 정의

---
+++ Delivery_Service/app/src/main/java/bu/ac/kr/delivery_service/presentation/trackingitems/TrackingItemsContract.kt
@@ -0,0 +1,27 @@
+package bu.ac.kr.delivery_service.presentation.trackingitems
+
+import bu.ac.kr.delivery_service.entity.TrackingInformation
+import bu.ac.kr.delivery_service.entity.TrackingItem
+import bu.ac.kr.delivery_service.presentation.BasePresenter
+import bu.ac.kr.delivery_service.presentation.BaseView
+
+class TrackingItemsContract {
+
+    interface View : BaseView<Presenter> {
+
+        fun showLoadingIndicator()
+
+        fun hideLoadingIndicator()
+
+        fun showNoDataDescription()
+
+        fun showTrackingItemInformation(trackingItemInformation: List<Pair<TrackingItem, TrackingInformation>>)
+    }
+
+    interface Presenter : BasePresenter {
+        var trackingItemInformation : List<Pair<TrackingItem, TrackingInformation>>
+
+        fun refresh()
+    }
+
+}
--- Delivery_Service/app/src/main/java/bu/ac/kr/delivery_service/presentation/trackingitems/TrackingItemsFragment.kt
+++ Delivery_Service/app/src/main/java/bu/ac/kr/delivery_service/presentation/trackingitems/TrackingItemsFragment.kt
@@ -1,6 +1,55 @@
 package bu.ac.kr.delivery_service.presentation.trackingitems
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import bu.ac.kr.delivery_service.databinding.FragmentTrackingItemsBinding
+import bu.ac.kr.delivery_service.entity.TrackingInformation
+import bu.ac.kr.delivery_service.entity.TrackingItem
+import org.koin.android.scope.ScopeFragment
 
-class TrackingItemsFragment : Fragment() {
+class TrackingItemsFragment : ScopeFragment(), TrackingItemsContract.View {
+
+    override val presenter: TrackingItemsContract.Presenter by inject()
+
+    private var binding : FragmentTrackingItemsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentTrackingItemsBinding.inflate(inflater)
+        .also{ binding = it}
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        presenter.onViewCreated()
+    }
+
+   /* override fun onDestroyView() {
+        super.onDestroyView()
+        presenter.onDestroyView()
+    }
+
+    override fun showLoadingIndicator() {
+        binding?.progressBar?.toVisible()
+    }
+
+    override fun hideLoadingIndicator() {
+        binding?.progressBar?.toGone()
+
+    }
+
+    override fun showNoDataDescription() {
+        binding?.refreshLayout?.toInvisible()
+        binding?.noDataContainer?.toVisible()
+    }
+
+    override fun showTrackingItemInformation(trackingItemInformation: List<Pair<TrackingItem, TrackingInformation>>) {
+    }*/
+
 }
