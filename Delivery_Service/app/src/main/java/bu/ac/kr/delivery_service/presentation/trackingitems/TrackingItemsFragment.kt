ca6afb293104d7dc3818cd57075abec71ff47437
jinahaEom <umjin98@naver.com>
2022-09-21 16:43:56 +0900

    오류 수정 및 택배 목록 화면 구성

--- Delivery_Service/app/src/main/java/bu/ac/kr/delivery_service/presentation/trackingitems/TrackingItemsFragment.kt
+++ Delivery_Service/app/src/main/java/bu/ac/kr/delivery_service/presentation/trackingitems/TrackingItemsFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import bu.ac.kr.delivery_service.R
 import bu.ac.kr.delivery_service.databinding.FragmentTrackingItemsBinding
 import bu.ac.kr.delivery_service.entity.TrackingInformation
 import bu.ac.kr.delivery_service.entity.TrackingItem
@@ -27,9 +31,12 @@ class TrackingItemsFragment : ScopeFragment(), TrackingItemsContract.View {
         .also{ binding = it}
         .root
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
+        bindView()
         presenter.onViewCreated()
     }
 
@@ -53,6 +60,29 @@ class TrackingItemsFragment : ScopeFragment(), TrackingItemsContract.View {
     }
 
     override fun showTrackingItemInformation(trackingItemInformation: List<Pair<TrackingItem, TrackingInformation>>) {
+        binding?.refreshLayout?.toVisible()
+        binding?.noDataContainer?.toGone()
+        (binding?.recyclerView?.adapter as? TrackingItemsAdapter)?.apply {
+            this.data = trackingItemInformation
+            notifyDataSetChanged()
+        }
+    }
+    private fun initViews(){  //View 초기화
+        binding?.recyclerView?.apply{
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = TrackingItemsAdapter()
+        }
+    }
+    private fun bindView(){
+        binding?.refreshLayout?.setOnRefreshListener {
+            presenter.refresh()
+        }
+        binding?.addTrackingItemButton?.setOnClickListener {
+            findNavController().navigate(R.id.action_tracking_items_dest_to_add_tracking_item_dest2)
+        }
+        binding?.addTrackingItemFloatingActionButton?.setOnClickListener { _ ->
+            findNavController().navigate(R.id.action_tracking_items_dest_to_add_tracking_item_dest2)
+        }
     }
 
 }
